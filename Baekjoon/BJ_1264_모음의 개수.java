6e9577c84f34557518bee567ede995c9f41e38d5
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-03-05 15:53:09 +0900

    Create BJ_1264_모음의 개수.java

---
+++ Baekjoon/BJ_1264_모음의 개수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            String s = br.readLine();
+            if(s.equals("#")){
+                break;
+            }
+            int temp = 0;
+            for(int i=0; i<s.length(); i++){
+                if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'||s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
+                    temp++;
+                }
+            }
+            sb.append(temp+"\n");
+        }
+        System.out.println(sb);
+    }
+}
