94ab51840742a2d4041e15f791810eab17287743
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-03-06 14:34:45 +0900

    Create BJ_2083_럭비 클럽.java

---
+++ Baekjoon/BJ_2083_럭비 클럽.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_2083_RugbyClub {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String name = st.nextToken();
+			int age = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			if (name.equals("#") && age == 0 && weight == 0) {
+				break;
+			}
+			if (age > 17 || weight >= 80) {
+				sb.append(name + " Senior\n");
+			} else {
+				sb.append(name + " Junior\n");
+			}
+		}
+		System.out.println(sb);
+
+	}
+
+}
