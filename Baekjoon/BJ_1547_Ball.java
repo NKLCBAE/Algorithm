c316d7bf6518563766a2132913c7156741dfe28d
NKLCBAE <87966378+NKLCBAE@users.noreply.github.com>
2023-05-28 23:11:10 +0900

    Create BJ_1547_Ball.java

---
+++ Baekjoon/BJ_1547_Ball.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int ans = 1;
+		for (int i = 0; i < M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int X = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			if (X == ans) {
+				ans = Y;
+			} else if (Y == ans) {
+				ans = X;
+			}
+		}
+		System.out.println(ans);
+
+	}
+
+}
